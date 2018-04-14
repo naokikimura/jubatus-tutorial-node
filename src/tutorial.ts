1a26b3c5954e89763b337895b7b9aa188df504e5
naokikimura <n.kimura.cap@gmail.com>
2018-04-14 17:10:47 +0900

    Update src/index.ts

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
+import bluebird from 'bluebird';
 import fs from 'fs';
+import jubatus from 'jubatus';
+import _ from 'lodash';
+import minimist from 'minimist';
 import path from 'path';
 import util from 'util';
-import jubatus from 'jubatus';
-import minimist from 'minimist';
-import bluebird from 'bluebird';
-import _ from 'lodash';
 
 const debug = util.debuglog('jubatus-tutorial-node');
 const { common: { types: { Datum } }, classifier: { types: { LabeledDatum }, client: { Classifier } } } = jubatus;
@@ -18,13 +18,14 @@ const { p: port, h: host, n: name, t: timeout, c: concurrency } = args;
 const classifier = new Classifier(port, host, name, timeout);
 
 const readFile = bluebird.promisify(fs.readFile);
+const readDir = bluebird.promisify(fs.readdir);
+const find = (dirname: string) => readDir(dirname).then(files => files.map(file => path.join(dirname, file)));
+const mapStat = (file: string, iteratee: (stat: fs.Stats, file: string) => string | any) =>
+    new Promise<string>((resolve, reject) => fs.stat(file, (error, stat) => error ? reject(error) : resolve(iteratee(stat, file))));
+const filterStat = (files: string[], predicate: (stat: fs.Stats) => boolean) =>
+    // tslint:disable-next-line:no-shadowed-variable
+    Promise.all(files.map(file => mapStat(file, stat => predicate(stat) && file))).then(files => files.filter(file => file));
 const list = (dirname: string) => {
-    const readDir = bluebird.promisify(fs.readdir);
-    const find = (dirname: string) => readDir(dirname).then(files => files.map(file => path.join(dirname, file)));
-    const mapStat = (file: string, iteratee: (stat: fs.Stats, file: string) => string | any) => 
-        new Promise<string>((resolve, reject) => fs.stat(file, (error, stat) => error ? reject(error) : resolve(iteratee(stat, file))));
-    const filterStat = (files: string[], predicate: (stat: fs.Stats) => boolean) => 
-        Promise.all(files.map(file => mapStat(file, (stat, file) => predicate(stat) && file))).then(files => files.filter(file => file));
     return find(dirname)
         .then(files => filterStat(files, stat => stat.isDirectory()))
         .then(directories => Promise.all(directories.map(directory => find(directory))))
@@ -35,9 +36,9 @@ const list = (dirname: string) => {
 classifier.clear().then(result => {
     debug('%s', result);
     return list('20news-bydate-train');
-}).then(labeledFiles => 
+}).then(labeledFiles =>
     // train
-    bluebird.map(labeledFiles, ({ label, file }) => 
+    bluebird.map(labeledFiles, ({ label, file }) =>
         readFile(file).then(buffer => {
             const message = buffer.toString();
             const datum = new Datum().addString('message', message);
@@ -49,13 +50,13 @@ classifier.clear().then(result => {
     const count = results.reduce((accumulator, current) => accumulator + current);
     debug('train results: %d', count);
     return list('20news-bydate-test');
-}).then(labeledFiles => 
+}).then(labeledFiles =>
     // classify
-    bluebird.map(labeledFiles, ({ label, file }) => 
+    bluebird.map(labeledFiles, ({ label, file }) =>
         readFile(file).then(buffer => {
             return classifier.classify([new Datum().addString('message', buffer.toString())]).then(results => {
                 return results.map(estimates => {
-                    const mostLikely = _.maxBy(estimates, (estimate) => estimate.score);
+                    const mostLikely = _.maxBy(estimates, estimate => estimate.score);
                     return ({ label, mostLikely, valid: _.get(mostLikely, 'label') === label });
                 });
             });
